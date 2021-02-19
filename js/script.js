23234908970f0293b90ce346eeef21dbaa277f8a
Theo-44 <muhiaalex@gmail.com>
2021-02-19 17:10:50 +0300

    add constant male and female names as well as days of the week

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+const maleName = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+
 letoutputTag = document.getElementById('output')
 
 
 function getName(event){
     event.preventDefault()
-    let date = document.
+    let date = document.getElementById('date').value
+    let gender = document.getElementById('gender').value
+    let myBirthday = new Date(date).toDateString()
+    let day = myBirthday.split(' ')[0]
+    
+    let message = `Born on:  ${day} you are ${gender} your name is ${name}`
+    outputTag.innerHTML = message
 }
