126e42a61b7b2ed8b7750052ec9e9d8e94d90d8c
Theo-44 <muhiaalex@gmail.com>
2021-02-22 02:23:02 +0300

    Add update to the span class Add update select function Add update to label function Add update h2 header

--- js/script.js
+++ js/script.js
@@ -1,53 +1,62 @@
 // The names listed below are ordered by days from Sunday
-const maleName = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-const femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
-femaleName[day]
-letoutputTag = document.getElementById('output')
-
-
-function getName(event){
+function getName(event) {
     event.preventDefault()
     let date = document.getElementById('date').value
     let gender = document.getElementById('gender').value
     let myBirthday = new Date(date).toDateString()
     let day = myBirthday.split(' ')[0]
-}
+    let name;
+    console.log(gender, date, myBirthday)
 
 
-}
-
-    let message = `Born on:  ${day} you are ${gender} your name is ${name}`
+    if (gender === 'female' && day === "Sun") {
+        name = fNames[0]
+        alert(`Your Akan name is: ${fNames[0]}`)
+    } else if (gender === 'female' && day === "Mon") {
+        name = fNames[1]
+        alert(`Your Akan name is: ${fNames[1]}`)
+    } else if (gender === 'female' && day === "Tue") {
+        name = fNames[2]
+        alert(`Your Akan name is: ${fNames[2]}`)
+    } else if (gender === 'female' && day === "Wed") {
+        name = fNames[3]
+        alert(`Your Akan name is: ${fNames[3]}`)
+    } else if (gender === 'female' && day === "Thu") {
+        name = fNames[4]
+        alert(`Your Akan name is: ${fNames[4]}`)
+    } else if (gender === 'female' && day === "Fri") {
+        name = fNames[5]
+        alert(`Your Akan name is: ${fNames[5]}`)
+    } else if (gender === 'female' && day === "Sat") {
+        name = fNames[6]
+        alert(`Your Akan name is: ${fNames[6]}`)
+    }
+    else if (gender === 'male' && day === "Sun") {
+        name = mNames[0]
+        alert(`Your Akan name is: ${mNames[0]}`)
+    } else if (gender === 'male' && day === "Mon") {
+        name = mNames[1]
+        alert(`Your Akan name is: ${mNames[1]}`)
+    } else if (gender === 'male' && day === "Tue") {
+        name = mNames[2]
+        alert(`Your Akan name is: ${mNames[2]}`)
+    } else if (gender === 'male' && day === "Wed") {
+        name = mNames[3]
+        alert(`Your Akan name is: ${mNames[3]}`)
+    } else if (gender === 'male' && day === "Thu") {
+        name = mNames[4]
+        alert(`Your Akan name is: ${mNames[4]}`)
+    } else if (gender === 'male' && day === "Fri") {
+        name = mNames[5]
+        alert(`Your Akan name is: ${mNames[5]}`)
+    } else if (gender === 'male' && day === "Sat") {
+        name = mNames[6]
+        alert(`Your Akan name is: ${mNames[6]}`)
+    }
+    let outputTag = document.getElementById("output")
+    let message = name
     outputTag.innerHTML = message
 }
-
-﻿
-if (gender =='m'){
-    alert(`You entered: ${dString}`)
-    alert(`Your Akan name is: ${mNames[day]}`
-
-} else{
-     alert(`You entered: ${dString}`)
-     alert(`Your Akan name is: ${fNames[day]}`
-}
-VM1108:3 Uncaught SyntaxError: missing ) after argument list
-if (gender =='m'){
-    alert(`You entered: ${dString}`)
-    alert(`Your Akan name is: ${mNames[day]}`)
-
-} else{
-     alert(`You entered: ${dString}`)
-     alert(`Your Akan name is: ${fNames[day]}`)
-}
-undefined
-let gender = 'f'
-undefined
-if (gender =='m'){
-    alert(`You entered: ${dString}`)
-    alert(`Your Akan name is: ${mNames[day]}`)
-
-} else{
-     alert(`You entered: ${dString}`)
-     alert(`Your Akan name is: ${fNames[day]}`)
-}
-undefined
